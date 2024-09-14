b84c1f319ecde2ab813ba8ccf0cd00eb5dc1276b
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-09-14 21:08:46 +0300

    add routs

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
 
-  app.use(express.json());
+  //   app.use(express.json());
   app.use(cors());
 
   app.use(
@@ -22,6 +23,32 @@ export const startServer = () => {
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello world!',
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      data: contacts,
+      message: 'Successfully found contacts!',
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({ message: 'Contact not found' });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      data: contact,
+      message: 'Successfully found contacts!',
     });
   });
 
